ebb6fb3367a2aac6b0fb1a885ea93ec0f9e2067b
Muhammad Rizky Sya'ban <13521119@std.stei.itb.ac.id>
2023-04-04 21:36:50 +0700

    modify interrupt and gdt

--- src/lib-header/gdt.h
+++ src/lib-header/gdt.h
@@ -5,6 +5,13 @@
 
 #define GDT_MAX_ENTRY_COUNT 32
 
+#define GDT_USER_CODE_SEGMENT_SELECTOR   0x18
+#define GDT_USER_DATA_SEGMENT_SELECTOR   0x20
+#define GDT_TSS_SELECTOR                 0x28
+
+// Set GDT_TSS_SELECTOR with proper TSS values, accessing _interrupt_tss_entry
+void gdt_install_tss(void);
+
 extern struct GDTR _gdt_gdtr;
 
 /**
