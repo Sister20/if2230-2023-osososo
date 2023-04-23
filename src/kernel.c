5a206dfdad4c972411f6f13abcf2ccf65f271c3c
SaddamAnnais <saddamannaiss@gmail.com>
2023-04-23 09:53:02 +0700

    feat: 3.2.6 done -> 3.2 done

--- src/kernel.c
+++ src/kernel.c
@@ -19,16 +19,45 @@ void kernel_setup(void) {
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
     initialize_filesystem_fat32();
-    keyboard_state_activate();
+    gdt_install_tss();
+    set_tss_register();
 
-    void *virtual_addr = (void *) 0x200000;
-    if (allocate_single_user_page_frame(virtual_addr) == 0) {
-        *((uint8_t*) 0x200000) = 5;
-    }
+    // Allocate first 4 MiB virtual memory
+    allocate_single_user_page_frame((uint8_t*) 0);
+     *((uint8_t*) 0) = 5;
 
-    
+    // Write shell into memory
+    struct FAT32DriverRequest request = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = "shell",
+        .ext                   = "\0\0\0",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+        .buffer_size           = 0x100000,
+    };
+    read(request);
 
-    
+    // Set TSS $esp pointer and jump into shell 
+    set_tss_kernel_current_stack();
+    kernel_execute_user_program((uint8_t*) 0);
+
+    while (TRUE);
+}
+
+
+// void kernel_setup(void) {
+//     enter_protected_mode(&_gdt_gdtr);
+//     pic_remap();
+//     initialize_idt();
+//     activate_keyboard_interrupt();
+//     framebuffer_clear();
+//     framebuffer_set_cursor(0, 0);
+//     initialize_filesystem_fat32();
+//     keyboard_state_activate();
+
+//     void *virtual_addr = (void *) 0x200000;
+//     if (allocate_single_user_page_frame(virtual_addr) == 0) {
+//         *((uint8_t*) 0x200000) = 5;
+//     }
 
     // struct ClusterBuffer cbuf[5];
     // for (uint32_t i = 0; i < 5; i++)
@@ -79,53 +108,5 @@ void kernel_setup(void) {
 
     // memcpy(newRequest.ext, "uwu", 3);
     // write(newRequest);
-    while (TRUE);
-}
-
-// void kernel_setup(void) {
-//     enter_protected_mode(&_gdt_gdtr);
-//     pic_remap();
-//     initialize_idt();
-//     activate_keyboard_interrupt();
-//     framebuffer_clear();
-//     framebuffer_set_cursor(0, 0);
-//     initialize_filesystem_fat32();
-//     keyboard_state_activate();
-
-//     struct ClusterBuffer cbuf[5];
-//     for (uint32_t i = 0; i < 5; i++)
-//         for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
-//             cbuf[i].buf[j] = i + 'a';
-
-//     struct FAT32DriverRequest request = {
-//         .buf                   = cbuf,
-//         .name                  = "ikanaide",
-//         .ext                   = "uwu",
-//         .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-//         .buffer_size           = 0,
-//     } ;
-
-//     write(request);  // Create folder "ikanaide"
-//     memcpy(request.name, "kano1\0\0\0", 8);
-//     write(request);  // Create folder "kano1"
-//     memcpy(request.name, "ikanaide", 8);
-//     memcpy(request.ext, "\0\0\0", 3);
-//     delete(request); // Delete first folder, thus creating hole in FS
-
-//     memcpy(request.name, "daijoubu", 8);
-//     request.buffer_size = 5*CLUSTER_SIZE;
-//     write(request);  // Create fragmented file "daijoubu"
-
-//     struct ClusterBuffer readcbuf;
-//     read_clusters(&readcbuf, ROOT_CLUSTER_NUMBER+1, 1); 
-//     // If read properly, readcbuf should filled with 'a'
-
-//     request.buffer_size = CLUSTER_SIZE;
-//     read(request);   // Failed read due not enough buffer size
-//     request.buffer_size = 5*CLUSTER_SIZE;
-//     read(request);   // Success read on file "daijoubu"
-
 //     while (TRUE);
 // }
-
-
